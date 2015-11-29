887203e75a726e8e57e87e3f98746f5144885ff9
Nadjib BELLOUNDJA <belloundjanadjib@gmail.com>
2015-11-29 23:36:10 +0100

    ajout prototype

--- orders.js
+++ orders.js
@@ -6,13 +6,33 @@ var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 var app = express();
 
+function Order(name, deliveryHour, orderNumber, status)
+{
+	this.name = name;
+	this.deliveryHour = deliveryHour;
+	this.orderNumber = orderNumber;
+	this.status = status;
+}
+
+Object.defineProperties(Order.prototype, {
+	"double": { get: function() { return "2"; }},
+	"finished" : {set: function(x) { this.status = x; }}
+});
+
+var sushiOrder = new Order("sushiOrder", "16h", 0, "pending");
+var soupOrder = new Order("soupOrder", "17h", 1, "pending");
+var saladOrder = new Order("saladOrder", "18h", 2, "pending");
+
+// Object.defineProperties(sushiOrder, {
+// 	"double": { get: function() { return "2"; }},
+// 	"finished" : {set: function(x) { this.status = x; }}
+// });
+
+
 app.use(session({secret: 'secret'}))
 .use(function(req, res, next) {
-	if (typeof(req.session.orderslist) === 'undefined')
-	{
-		console.log("added orders list");
-		req.session.orderslist = ["soupe poireux", "sushis", "pizza bio"];
-	}
+	if (typeof(req.session.orderslist) !== 'undefined')
+		req.session.orderslist = [sushiOrder, soupOrder, saladOrder];
 	next();
 })
 
@@ -22,7 +42,9 @@ app.get('/orderslist', function(req, res) {
 })
 .get('/orderslist/finish/:id', function(req, res) {
 	if (req.params.id != '')
-		req.session.orderslist.splice(req.params.id, 1);
+		req.session.orderslist[req.params.id].finished = "finished";
+		// req.params.id.finished;
+		// req.session.orderslist.splice(req.params.id, 1);
 	res.redirect('/orderslist');
 })
 
